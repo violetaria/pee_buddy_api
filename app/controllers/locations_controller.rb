1eabec70b6c3d0b6ecc7d90c88ede40ab48b185a
violetaria <terri.chu@outlook.com>
2016-10-02 17:16:07 -0400

    index for locations, hook up to places service

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,18 @@
+class LocationsController < ApplicationController
+
+  def index
+    type = 'gas_station'
+    results = GooglePlacesApi.nearby_search(params[:lat],params[:lng],type)
+    @locations = results.map do |result|
+      Location.includes(:ratings).find_or_create_by!(place_id: result['place_id'],
+                                  lat: result['geometry']['location']['lat'],
+                                  lng: result['geometry']['location']['lng'],
+                                  name: result['name'])
+    end
+    render 'index.json.jbuilder', status: :ok
+  end
+
+  def show
+
+  end
+end
