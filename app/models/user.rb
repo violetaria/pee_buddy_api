192338c2670462355f9f2c577c373a71b1797d5b
violetaria <terri.chu@outlook.com>
2017-03-12 16:24:31 -0400

    annotate gem, update to files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                   :integer          not null, primary key
+#  first_name           :string
+#  last_name            :string
+#  email                :string
+#  uid                  :string
+#  provider             :integer
+#  authentication_token :string           not null
+#  password_digest      :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
 class User < ApplicationRecord
   has_secure_password
 
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id         :integer          not null, primary key
+#  place_id   :string           not null
+#  lat        :float            not null
+#  lng        :float            not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
