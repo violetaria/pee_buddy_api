c6a6a6c663987410751c40ef0c48cc418c84ec24
violetaria <terri.chu@outlook.com>
2017-03-12 16:30:08 -0400

    fix tables up, update models

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -8,12 +8,17 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  user_id     :integer          not null
-#  category    :integer          default(1)
+#  rating_type :integer
 #
 
 class Rating < ApplicationRecord
   belongs_to :rating_type
   belongs_to :location
+  belongs_to :user
 
   validates_presence_of  :rating, :rating_type, :location
+
+  enum rating_type: {
+    overall: 1
+  }
 end
--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -8,7 +8,7 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  user_id     :integer          not null
-#  category    :integer          default(1)
+#  rating_type :integer
 #
 
 require 'test_helper'
