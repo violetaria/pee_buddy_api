d29f98cb38e318f469e9098e9cbd40a44acf0cac
violetaria <terri.chu@outlook.com>
2017-07-23 12:15:13 -0400

    fix migration

--- db/migrate/20170312202807_remove_category_add_type_to_ratings.rb
+++ db/migrate/20170312202807_remove_category_add_type_to_ratings.rb
@@ -1,5 +1,6 @@
 class RemoveCategoryAddTypeToRatings < ActiveRecord::Migration[5.0]
   def change
+    remove_column :ratings, :rating_type, :references
     drop_table :rating_types
     remove_column :ratings, :category, :integer
     add_column :ratings, :rating_type, :integer
