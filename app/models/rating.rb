632578dfbbd78902b41cef79d08d2763882f7272
violetaria <terri.chu@outlook.com>
2017-08-12 20:38:21 -0400

    update uniqueness for duplicate ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -16,6 +16,7 @@ class Rating < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :rating, :rating_type, :location_id, :user_id
+  validates_uniqueness_of :rating, scope: [:location_id, :user_id, :rating_type], message: "Oh no! You've already let loose a fury of ratings on this toilet!"
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 
   enum rating_type: {
